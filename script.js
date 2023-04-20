97fdfe4cb643cf75cc212c8daef4541d90fcfff1
Whitessx <89744868+Whitessx@users.noreply.github.com>
2023-04-20 02:57:15 +0200

    Added some meta tags, and a footer.

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
+function getCurrentYear() {
+  const currentYear = new Date().getFullYear();
+  return currentYear;
+}
+
+function updateFooterText() {
+  const footerText = document.getElementById("footer-text");
+  const currentYear = getCurrentYear();
+  footerText.innerHTML = `©${currentYear} Limpiezas Nancy. Todos los derechos reservados.`;
+}
+
+updateFooterText();
+
+document.addEventListener("click", function (event) {
+  const target = event.target;
+  const hamburgerBtn = document.getElementById("hamburger-btn");
+  const navbarNav = document.getElementById("navbarNav");
+  const isNavOpen = navbarNav.classList.contains("show");
+
+  if (
+    !target.closest(".navbar-toggler") &&
+    !target.closest("#navbarNav") &&
+    isNavOpen
+  ) {
+    hamburgerBtn.click();
+  }
+});
+
 // ScrollReveal().reveal(".card-reveal", { delay: 500 });
 // ScrollReveal().reveal(".card-reveal2", { delay: 800 });
